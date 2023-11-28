132b665360d79bd77b9d2ef79f9258038a70ac5f
pkyu <55111674+pkyu@users.noreply.github.com>
2023-11-28 18:48:07 -0500

    Working on inventory table stuff

--- src/components/InventoryTable.tsx
+++ src/components/InventoryTable.tsx
@@ -34,6 +34,7 @@ export default function InventoryTable() {
   const [inventory, setInventory] = useState<any[] | []>([]);
 
   useEffect(() => {
+    updateInventory();
     fetchInventory();
   }, [])
 
@@ -77,6 +78,91 @@ export default function InventoryTable() {
         alert((error as Error).message);
     }
   }
+  async function updateInventory() {
+    console.log("Checking esp32_barcodes table for any entries since last update...")
+    try {
+      let { data, error } = await supabase
+          .from('esp32_barcodes')
+          .select('*');
+      if (error) { 
+        alert('Error checking esp32_barcodes table: ' + error);
+        throw error;
+      }
+      if (data) {
+        if (data.length == 0) console.log("No new entries found");
+        else {
+          let tableEntries: Array<{scan_mode: string, barcode: number}> = data;
+          for (let i = 0; i < 1 ; i++) {
+            try {
+              console.log("New entry: " + tableEntries[i]); 
+              let { data, error } = await supabase
+                  .from('products')
+                  .select('*')
+                  .eq('barcode', tableEntries[i].barcode);
+              if (error) { throw error; }
+              if (data) {
+                if (data.length == 0) {
+                  if (tableEntries[i].scan_mode === "remove") {
+                    console.log("Tried to remove non-existing inventory, no action taken")
+                  }
+                  else {
+                    console.log("Product not found in table, retrieving name from food API")
+                    let foodInfo = await fetch("https://world.openfoodfacts.org/api/v2/product/" + tableEntries[i].barcode).then(res => res.json());
+                    console.log(foodInfo);
+                    console.log("Adding product to table");
+                    
+                  }
+                }
+                else {
+                  if (tableEntries[i].scan_mode === "remove") {
+
+                  }
+                  else {
+                    console.log("Product found in table, raising quantity by one.")
+                    let newQuantity = data[0].quantity + 1;
+                    try {
+                      let { error } = await supabase
+                          .from('products')
+                          .update([{quantity: newQuantity}])
+                          .eq('barcode', tableEntries[i].barcode);
+                      if (error) { throw error }
+                    } catch (error) {
+                    alert((error as Error).message);
+                    }
+                  }
+                }
+              }
+          } catch (error) {
+              alert((error as Error).message);
+          }
+          }
+        }
+      }
+  } catch (error) {
+      alert((error as Error).message);
+  }
+  }
+
+  const addProductBarcode = async (barcode: string | number) => {
+    try {
+        // First try to fetch product information from the food info table
+
+        // Insert the new item, providing the item name and barcode; the rest gets filled in automatically
+        let { data, error } = await supabase
+            .from('products_duplicate')
+            .insert([{ name: "hi", barcode }]);
+
+        // Handle any errors.
+        if (error) { throw error; }
+        
+        // Upon success, update the inventory
+        if (data) {
+            await fetchInventory(); // Assuming fetchInventory is an async function, await it here
+        }
+    } catch (error) {
+        alert((error as Error).message);
+    }
+}
 
   return (
     <div>
