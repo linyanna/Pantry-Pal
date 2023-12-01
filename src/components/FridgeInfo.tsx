e940a2c29076f89a445fa417f326df41f97a1c13
Skylar Stewart <73617168+SkylarStewart@users.noreply.github.com>
2023-12-01 02:31:41 -0500

    fixed the FridgeInfo alignment and replaced w/ minecraft chest (#14)

--- src/components/FridgeInfo.tsx
+++ src/components/FridgeInfo.tsx
@@ -1,7 +1,10 @@
 'use client'
 import { useState, useEffect } from 'react'
 import { supabase } from '../lib/utils/supabase/supabaseConnection';
+import { useTheme } from "next-themes";
 import './FridgeInfo.css';
+import chest_closed from '../images/chest_closed.png'
+import chest_open from '../images/chest_open.png'
 
 export default function FridgeInfo() {
 
@@ -29,21 +32,32 @@ export default function FridgeInfo() {
     return () => supabase.removeChannel(subscription);
   }, [])
 
+  //theme hook
+  const { theme } = useTheme();
+
   return (
     <div className="info-container">
       {isOpen
         ? (
           <div className='fridge-info-container'>
             <div className='fridge-info-header'>
-              <div className='fridge-info-open'></div>
-              <p className="fridge-info-text">the fridge is open.</p>
+              <img src={chest_open.src} alt="Example" style={{ width: '50px', height: '50px', marginTop: '-15px', marginRight: '10px' }} />
+              <p style={{
+                color: theme === 'dark' ? 'white' : 'black',
+                marginTop: '8px',
+              }}>the door is open.</p>
             </div>
-
-          </div>)
+          </div>
+        )
         : (
-          <div>
-            <div className='fridge-info-closed' ></div>
-            <p className="fridge-info-text">the fridge is closed.</p>
+          <div className='fridge-info-container'>
+            <div className='fridge-info-header'>
+              <img src={chest_closed.src} alt="Example" style={{ width: '50px', height: '50px', marginTop: '-15px' }} />
+              <p style={{
+                color: theme === 'dark' ? 'white' : 'black',
+                marginTop: '8px',
+              }}>the door is closed.</p>
+            </div>
           </div>
         )
       }
